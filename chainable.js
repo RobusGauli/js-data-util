77c1a815bd47fede0f9e3e1b641a551d2baeaedc
Robus Gauli <robusgauli@gmail.com>
2018-03-11 13:49:35 +0545

    chainalbe

---
+++ chainable.js
@@ -0,0 +1,44 @@
+// this techinique will be much more better to expose the api 
+
+// this is also used in library such as Prop Types
+
+
+const makeObjectBasedOnClosure = (a=true, b=true) => {
+  const obj =  new class {
+    constructor() {
+      // this a nd b variable are from the function closure rather than passing
+      // from the constructor directly
+      this.a = a;
+      this.b = b;
+    }
+
+    doSomething() {
+      if (this.a) {
+        // od something
+      } else {
+        // do another thing
+      }
+    }
+
+  }();
+  makeObjectBasedOnClosure.falsy = makeObjectBasedOnClosure.bind(
+    null,
+    false,
+    false
+  );
+  return obj;
+}
+
+
+const main = () => {
+  // creating an instance based on different vairable based on diffenent values
+  // using function chaining
+  let c = makeObjectBasedOnClosure()
+  console.log(c);
+  let d = makeObjectBasedOnClosure.falsy();
+  console.log(d);
+  
+}
+
+main();
+
